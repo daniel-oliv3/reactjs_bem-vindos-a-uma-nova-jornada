bc42c8658ce77740650c403267e82695cb232bd4
Daniel Oliveira <danielsaapup3@gmail.com>
2023-06-27 09:28:52 -0400

    EXERCÍCIO 07 COM USESTATE

--- app-reactjs_22/src/components/UseState_09.jsx
+++ app-reactjs_22/src/components/UseState_09.jsx
@@ -7,10 +7,22 @@ import React, { useState } from "react";
 // ------- Componente -------
 function UseState_09() {
 
+  const [login, setLogin] = useState(false);
+  const [btnText, setBtnText] = useState('Login');
+
   return (
     <>
       <h3>Exercício 08 - Conditional rendering - Fake Login</h3>
       <hr/>
+
+
+      <button onClick={() => {
+        setLogin(!login);
+        setBtnText(login ? 'Login' : 'Logout');
+      }}>{btnText}</button>
+
+
+      {login && <p>Usuário logado...</p>}
 
     </>
   );
