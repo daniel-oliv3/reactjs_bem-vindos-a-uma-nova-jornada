e63a32f1ae08bb778b330821f38440fb8447454d
Daniel Oliveira <danielsaapup3@gmail.com>
2023-06-28 10:35:13 -0400

    EXERCÍCIO 08 COM USESTATE

--- app-reactjs_23/src/components/UseState_10.jsx
+++ app-reactjs_23/src/components/UseState_10.jsx
@@ -1,4 +1,4 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
 
 
@@ -7,11 +7,22 @@
 // ------- Componente -------
 function UseState_10() {
 
+  const[tab, setTab] = useState(1);
+
   return (
     <>
-      <h3>Exercício 09</h3>
+      <h3>Exercício 09 - Conditional rendering - Visible Tab</h3>
       <hr/>
 
+      <select onChange={(e) =>  setTab(parseInt(e.target.value)) }>
+        <option value="1">Tab 1</option>
+        <option value="2">Tab 2</option>
+        <option value="3">Tab 3</option>
+      </select>
+
+      {tab === 1 && <p>Tab 1</p>}
+      {tab === 2 && <p>Tab 2</p>}
+      {tab === 3 && <p>Tab 3</p>}
 
     </>
   );
