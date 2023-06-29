650c6e10270c6d29d4048231da9592c71eeebc11
Daniel Oliveira <danielsaapup3@gmail.com>
2023-06-29 08:53:18 -0400

    EXERCÍCIO 09 COM USESTATE

--- app-reactjs_24/src/components/UseState_11.jsx
+++ app-reactjs_24/src/components/UseState_11.jsx
@@ -1,4 +1,5 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
+import '../components/UseState_11.css';
 
 
 
@@ -7,12 +8,18 @@
 // ------- Componente -------
 function UseState_11() {
 
+  const [state, setState] = useState(true);
+  const class1 = "classUm";
+  const class2 = "classDois";
+
   return (
     <>
-      <h3>Exercício 10 - Conditional rendering - ...</h3>
+      <h3>Exercício 10 - Conditional rendering - Change CSS class</h3>
       <hr/>
 
+      <button onClick={() => setState(!state) }>Alterar Parágrafo</button>
       
+      <p className={`paragrafo ${state ? class1 : class2}`}>Texto que vai ter seu aspecto alterado!</p>
 
     </>
   );
