8444d5a9fe53687992c87e06056bfeca48fca082
Daniel Oliveira <danielsaapup3@gmail.com>
2023-07-01 09:23:21 -0400

    EXERCÍCIO 11 COM USESTATE

--- app-reactjs_26/src/components/UseState_13.jsx
+++ app-reactjs_26/src/components/UseState_13.jsx
@@ -1,4 +1,6 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
+
+
 
 
 
@@ -7,14 +9,19 @@
 // ------- Componente -------
 function UseState_13() {
 
+  const [text, setText] = useState("");
+  // const [text, setText] = useState("Texto inicial");
   
   return (
     <>
-      <h3>Exercício 12 - Conditional rendering - </h3>
+      <h3>Exercício 11 - Input data</h3>
       <hr/>
 
-    
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
 
+      <p>{text}</p>
+      <h3>{text}</h3>
+      <p>O texto inserido é : {text}</p>
       
       
     </>
