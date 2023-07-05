11d406ab947d1ffbe16ecbccf23dfcda7c08d231
Daniel Oliveira <danielsaapup3@gmail.com>
2023-07-05 09:26:59 -0400

    EXERCÍCIO 12 COM USESTATE

--- app-reactjs_27/src/components/UseState_14.jsx
+++ app-reactjs_27/src/components/UseState_14.jsx
@@ -1,4 +1,4 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
 
 
@@ -9,13 +9,30 @@
 // ------- Componente -------
 function UseState_14() {
 
+  const [text, setText] = useState("");
+  const [nomes, setNomes] = useState([]);
+
+  function inserir(){
+    setNomes(prev => [...prev, text]);
+    setText('');
+  }
   
   return (
     <>
-      <h3>Exercício 12 - </h3>
+      <h3>Exercício 12 - Input data - Collection</h3>
       <hr/>
 
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={inserir}>Inserir</button>
 
+      
+      <ul>
+          {
+            nomes.map((data, index) => (
+              <li key={index}>{data}</li>
+            ))
+          }
+      </ul>
       
     </>
   );
